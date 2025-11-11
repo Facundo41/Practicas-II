07afbf4cea0fb0d2d0afaa8dfe0ee9d5da295669
Mateo Juarez <mateoporo1234@gmail.com>
2025-11-11 10:13:39 -0600

    Agregada sección de Preguntas Frecuentes

---
+++ Practicas-II-main/JAVASCRIPT/script.js
@@ -0,0 +1,24 @@
+// Efecto suave al hacer scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      target.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
+
+// Animación al cargar
+window.addEventListener("load", () => {
+  document.body.style.opacity = 1;
+});
+
+
+const form = document.getElementById('contactForm');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert("mensaje enviado");
+    form.reset();
+});
